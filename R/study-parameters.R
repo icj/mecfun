83b6ca8a54f70a179bb81a471991b9b9adfc9154
icj <ijenkins@fredhutch.org>
2019-09-23 09:55:40 -0700

    added study_ancestry parameters

--- R/study-parameters.R
+++ R/study-parameters.R
@@ -1,5 +1,5 @@
 
-#' Ethnicity Study Parameters
+#' Self-report Ethnicity Study Parameters
 #'
 #' Set global variables for the MEC Ethnicity paper
 #' @param main_var Main study categorical variable
@@ -46,6 +46,77 @@ study_ethnicity <- function(main_var = "Q1_eth",
                                             "Smoking"),
                             run_code = FALSE,
                             download_files = TRUE) {
+  main_var <<- main_var
+  main_var_cols <<- main_var_cols
+  main_cont <<- main_cont
+  dir_out <<- dir_out
+  dir_dta <<- dir_dta
+  dir_dwn <<- dir_dwn
+  dir_fig <<- dir_fig
+  dir_tab <<- dir_tab
+  study_filter <<- study_filter
+  study_title <<- study_title
+  sg <<- sg
+  q_cut <<- q_cut
+  zhern_cols <<- zhern_cols
+  names(zhern_cols) <<- zhern_names
+
+  # Book build
+  run_code <<- run_code
+  download_files <<- download_files
+}
+
+#' Genetic Ancestry Study Parameters
+#'
+#' Set global variables for the MEC Characterization paper
+#' @param main_var Main study categorical variable
+#' @param main_var_cols Colors for main study varible
+#' @param main_cont Main study continuous variable
+#' @param dir_out Output directory
+#' @param dir_dta Output directory for data objects
+#' @param dir_dwn Output directory for downloadable files
+#' @param dir_fig Output directory for figures
+#' @param dir_tab Output directory for tables
+#' @param study_filter Study indicator for filtering to appropriate samples
+#' @param study_title Formal study title
+#' @param sg Lowercase study group name
+#' @param q_cut FDR cutoffs for phylum and genera
+#' @param zhern_cols Colors for Zhernakova categories
+#' @param zhern_names Names of Zhernakova categories
+#' @param run_code Run scripts during book build
+#' @param download_files Create downloadable file links
+#'
+#' @return Variables are saved to the global environment.
+#' @export
+study_ancestry <- function(main_var = "anc_max",
+                           main_var_cols = c("#A6CEE3", "#1F78B4",
+                                             "#B2DF8A", "#33A02C",
+                                             "#FB9A99", "#E31A1C",
+                                             "#FF7F00",
+                                             "#6A3D9A"),
+                           main_cont = "",
+                           dir_out = "Output/Ancestry",
+                           dir_dta = file.path(dir_out, "Data"),
+                           dir_dwn = file.path(dir_out, "Downloads"),
+                           dir_fig = file.path(dir_out, "Figures"),
+                           dir_tab = file.path(dir_out, "Tables"),
+                           study_filter = "mGWAS",
+                           study_title = "Ancestry",
+                           sg = "ancestry",
+                           q_cut = c(phylum = 0.05, genera = 0.05),
+                           zhern_cols = viridis::viridis(10),
+                           zhern_names = c("Demographics",
+                                           "Alcohol",
+                                           "Anthropometric measures",
+                                           "Special diet",
+                                           "Cultural foods & dietary components",
+                                           "Dietary quality index",
+                                           "Medical conditions",
+                                           "Medications",
+                                           "Physical activity",
+                                           "Smoking"),
+                           run_code = FALSE,
+                           download_files = TRUE) {
   main_var <<- main_var
   main_var_cols <<- main_var_cols
   main_cont <<- main_cont
